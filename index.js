d59311e3f62118f882a8555dc7804168b6a531ed
chenwenxiu <wenxiu.cox@alibaba-inc.com>
2017-06-30 11:12:59 +0800

    add test case, improved coverage to 100%

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const postcss = require('postcss')
 const parseValue = require('postcss-value-parser')
 const Datauri = require('datauri')
 const { dirname, resolve } = require('path')
+const fs = require('fs')
 
 module.exports = postcss.plugin('__inline', () => (root, result) => {
   const cssDirname = dirname(root.source.input.file)
@@ -12,13 +13,15 @@ module.exports = postcss.plugin('__inline', () => (root, result) => {
       if (node.type === 'function' && node.value === 'url') {
         if (node.nodes.length === 1) {
           const url = node.nodes[0]
-          if (url.type === 'word' || url.type === 'string') {
-            const urlValue = url.value
-            const match = urlValue.match(/^(.+)\?__inline$/)
-            if (match) {
-              const file = resolve(cssDirname, match[1])
+          const urlValue = url.value
+          const match = urlValue.match(/^(.+)\?__inline$/)
+          if (match) {
+            const file = resolve(cssDirname, match[1])
+            if (fs.existsSync(file)) {
               url.value = new Datauri(file).content
               processed = true
+            } else {
+              throw new Error('File: ' + file + ' not exists')
             }
           }
         }
--- index.test.js
+++ index.test.js
@@ -14,3 +14,22 @@ it('should inline binary files', () => Promise.all([
 ]).then(([result, expectCss]) => {
   expect(result.css).toBe(expectCss)
 }))
+
+it('file not exists', () => {
+  readFile('fixtures/not-exists/style.css', 'utf8').then(
+    code => postcss([__inline]).process(code, {
+      from: 'fixtures/not-exists/style.css'
+    }))
+    .catch((e) => {})
+})
+
+it('not standard', () => Promise.all([
+  readFile('fixtures/not-standard/style.css', 'utf8').then(
+    code => postcss([__inline]).process(code, {
+      from: 'fixtures/not-standard/style.css'
+    })
+  ),
+  readFile('fixtures/not-standard/expect.css', 'utf8')
+]).then(([result, expectCss]) => {
+  expect(result.css).toBe(expectCss)
+}))
